24b2b5f8f59b40bac409456bc85c9d7c20006496
unknown <stuartalafollette@gmail.com>
2018-05-15 18:51:31 -0500

    initial commit with a few basic things

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+console.log("working");
+
+// var queryUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=jJX05QvgTa2nRHTUMe5HRhDb69Jz8PftUHrgmNni&location=Denver+CO";
+// var apiKey = "jJX05QvgTa2nRHTUMe5HRhDb69Jz8PftUHrgmNni";
+
+
+
+// $("#test-btn").on("click", function(){
+
+//     $.ajax({
+//         url:
+//     })
+// })
